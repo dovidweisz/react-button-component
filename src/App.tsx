305183dd183a06c3e70a434992660917b8b71cd7
dovidweisz <dave@dweisz.com>
2018-12-06 17:40:59 -0500

    Start using prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import * as styles from './App.scss';
+import * as React from "react";
+import * as styles from "./App.scss";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className={styles.app}>
-        <header className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1 className={styles.appTitle}>Welcome to React</h1>
-        </header>
-        <p className={styles.appIntro}>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	public render() {
+		return (
+			<div className={styles.app}>
+				<header className={styles.appHeader}>
+					<img src={logo} className={styles.appLogo} alt="logo" />
+					<h1 className={styles.appTitle}>YO! Welcome to React</h1>
+				</header>
+				<p className={styles.appIntro}>
+					To get started, edit <code>src/App.tsx</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
