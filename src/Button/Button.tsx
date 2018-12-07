407ea648cdb12d45eb350a26675f7dfcd087f2a0
dovidweisz <dave@dweisz.com>
2018-12-07 09:42:32 -0500

    Button.tsx

---
+++ src/Button/Button.tsx
@@ -0,0 +1,84 @@
+import classNames from "classnames";
+import React, {
+	ButtonHTMLAttributes,
+	HTMLAttributes,
+	InputHTMLAttributes,
+} from "react";
+import { withStyles } from "src/client/App/common/WithStyleAndLocale";
+
+const classes: {
+	[key: string]: string;
+} = require("./button.scss");
+
+export type buttonColor =
+	| "primary"
+	| "primaryAlt"
+	| "secondary"
+	| "tertiary"
+	| "tertiaryAlt";
+
+export interface IButtonBaseProps {
+	color?: buttonColor;
+	ghost?: boolean;
+	disabled?: boolean;
+	small?: boolean;
+	width?: "wide" | "xWide" | "fitWidth";
+	large?: boolean;
+}
+
+export type IButtonProps = IButtonBaseProps &
+	ButtonHTMLAttributes<HTMLButtonElement>;
+export type IInputButtonProps = IButtonBaseProps & {
+	type?: "button" | "submit";
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const _ButtonBase: React.SFC<
+	IButtonBaseProps &
+		HTMLAttributes<HTMLElement> & { children: (props: any) => any }
+> = ({
+	color,
+	ghost,
+	disabled,
+	small,
+	width,
+	large,
+	className,
+	children,
+	...otherAttributes
+}) => {
+	const colorClass = color ? classes[color] : "";
+	const widthClass = width ? classes[width] : "";
+	const parentClassName = className ? className : "";
+	className = classNames([
+		classes.button,
+		colorClass,
+		parentClassName,
+		{
+			[classes.disabled]: disabled,
+			[classes.ghost]: ghost,
+			[classes.small]: small,
+			[widthClass]: !small && !!width,
+			[classes.large]: !small && !!large,
+		},
+	]);
+	return children({ ...otherAttributes, className, disabled });
+};
+
+export const ButtonBase = withStyles(classes)(_ButtonBase);
+
+export const Button = (props: IButtonProps) => {
+	const { children, ...otherAttributes } = props;
+	return (
+		<ButtonBase {...otherAttributes}>
+			{props => <button {...props}>{children}</button>}
+		</ButtonBase>
+	);
+};
+
+export const InputButton = (props: IInputButtonProps) => {
+	return (
+		<ButtonBase {...props}>
+			{props => <input type="button" {...props} />}
+		</ButtonBase>
+	);
+};
